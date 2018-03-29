99e2058591079eb3cf8eded23b12f648fac8456e
ehsankakaei91 <ehsankakaei91@gmail.com>
2018-03-29 17:13:40 +0200

    example data and atlas created and aal atlas has been removed

--- examples/demo.m
+++ examples/demo.m
@@ -10,12 +10,12 @@ clc
 
 addpath(genpath(fullfile('..','lib')))
 %% fitting to atlas
-atlasnii = fullfile('..','atlas','aal','ROI_MNI_V4.nii'); % path to atlas nifti file
-atlaslist = fullfile('..','atlas','aal','ROI_MNI_V4_List.mat'); % path to list of ROI
+atlasnii = fullfile('.','Example_Atlas','exampleatlas.nii'); % path to atlas nifti file
+atlaslist = fullfile('.','Example_Atlas','exampleatlas_List.mat'); % path to list of ROI
 nfiles = 2; 
 for n = 1:nfiles
-imagepath = fullfile('.','Example_Data',['example',num2str(n),'.nii']); % path to images
-save_image_name = ['example',num2str(n)];
+imagepath = fullfile('.','Example_Data',['exampledata',num2str(n),'.nii']); % path to images
+save_image_name = ['example',num2str(n),'_atlas_fitted'];
 
 out_data = img2atlas(atlasnii,atlaslist,imagepath,'save',save_image_name);
 
@@ -24,7 +24,7 @@ end
 
 %% local correlation
 TR_range = 17:166; % TRs used for analysis
-region = 65; % region code (e.g. 65 for Angular L)
+region = 1; % region code (e.g. 1 for Angular L)
 save_correlation_name = fullfile('.','Example_Data','correlation_profile');
 
 [rho,pval,zscore] = local_corr(region,images,TR_range,'save',save_correlation_name);
@@ -41,14 +41,14 @@ nvoxels = size(R,1);
 voxel_per_cluster = 200;
 nclusters = floor(nvoxels/voxel_per_cluster);
 [Idx, Tidx, nc,Dis] = ClusterWithKmeans(R_th,nclusters);
-save(['cluster' num2str(region)],'Idx')
+save(fullfile('.','Example_Data',['cluster' num2str(region)]),'Idx')
 
 %% save atlas list and nifti
-clusters(1) = {['cluster' num2str(region)]};
+clusters(1) = {fullfile('.','Example_Data',['cluster' num2str(region)])};
 [atlas_data,ROI] = cluster2atlas(clusters,atlasnii,atlaslist,region ...
     ,'save','example_atlas','nii','example_atlas.nii');
 %% Plots
-% sorting by cluster
+% sorting by clusters
 R1 = rho{1};
 R2 = rho{2};
 index = cell(1,nc);
@@ -65,16 +65,16 @@ Rsort2 = Rsort2(A_all,:);
 figure
 subplot(2,2,1)
 surf(R1,'EdgeColor','none');view(2);axis equal; axis ij; axis off
-title('example 1')
+title('native example data 1')
 
 subplot(2,2,2)
 surf(R2,'EdgeColor','none');view(2);axis equal; axis ij; axis off
-title('example 2')
+title('native example data 2')
 
 subplot(2,2,3)
 surf(Rsort1,'EdgeColor','none');view(2);axis equal; axis ij; axis off
-title('example 1 sorted')
+title('example data 1 sorted by clusters')
 
 subplot(2,2,4)
 surf(Rsort2,'EdgeColor','none');view(2);axis equal; axis ij; axis off
-title('example 2 sorted')
+title('example data 2 sorted by clusters')
